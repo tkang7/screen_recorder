b9956527cc9990aa0eb54fb46f08df7253b133d9
David Kang <tk.kang@outlook.com>
2023-12-12 11:36:09 -0500

    Created electron app and added some buttons to it - no JS yet

---
+++ my-app/src/render.js
@@ -0,0 +1,7 @@
+// Buttons
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementsById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+
+
